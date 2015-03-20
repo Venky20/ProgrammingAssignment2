a39df65b0650eb4371d73a40ed2fc0718fb0d608
Venky Sivaraman <venky.sivaraman@gmail.com>
2015-03-20 17:08:41 +0530

    Few changes and added some more comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 ## Caching will help us to avoid the time consuming calculations again and again, 
 ##   instead it can be directly accessed from memory  if it is cached. 
 
-## The following 'makeCacheMatrix' function creates a special "matrix" object that can cache its inverse.
+## The following 'makeCacheMatrix' function creates a "matrix" object that can cache its inverse.
 #1.  set the value of the matrix
 #2.  get the value of the matrix
 #3.  set the value of the inverse
@@ -22,10 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
   ## Get the matrix
   get <- function() x
   
-  ## Set or Cache the matrix.
+  ## Set (or Cache) the inversed matrix.
   setinv <- function(solve) i <<- solve
   
-  ## Get the cached matrix
+  ## Get the cached inverse matrix
   getinv <- function() i
   list(set = set, get = get, setinv = setinv, getinv = getinv)
   
@@ -33,7 +33,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## The following `cacheSolve` function computes the inverse of the special "matrix" returned by `makeCacheMatrix` above. 
 ##  If the inverse has already been calculated (and the matrix has not changed), 
-##    then`cacheSolve` should retrieve the inverse from the cache.
+##    then`cacheSolve` will retrieve the inverse from the cache.
 
 ## At the moment giving: Error in x$getinv : $ operator is invalid for atomic vectors
 cacheSolve <- function(x, ...) { 
